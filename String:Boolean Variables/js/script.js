20376facdbaae613e776893bac293b9bbb5f5616
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-01 18:40:44 -0400

    Boolean/Strings
    Learning about the structure of Booleans and Strings

---
+++ String:Boolean Variables/js/script.js
@@ -0,0 +1,9 @@
+//Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 38;
+
+var isStudent = false;
+
+var firstName = "Kimerlee";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
