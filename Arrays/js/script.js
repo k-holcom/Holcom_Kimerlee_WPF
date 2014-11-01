29672811539926f057ed6dcb5cdcc3d2594cfceb
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-01 18:56:02 -0400

    Learning Arrays
    Followed Screencast for Arrays

---
+++ Arrays/js/script.js
@@ -0,0 +1,16 @@
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; //0, 1, 2, 3
+
+//console.log(muppetNames[1]);// Lets me see whats in the index #1
+
+muppetNames[1] = "Rizzo"; // change index#1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+//console.log(muppetNames[2]);
+
+//console.log(muppetNames[1]);
+
+var num = 1;
+
+console.log(muppetNames[num]);
