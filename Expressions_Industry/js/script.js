2b79ae01bd9ea7bac17f0599550ae07f87f580bb
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 17:40:20 -0500

    Calculate height in pixels
    Used the *= to multiply the height by the standardDPI

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -10,12 +10,13 @@
 //the variables needed are height, width, pixels for each, and percentage that the user wants to know the number of pixels of.
 var height;
 var width;
-var heightInPixels;
-var widthInPixels;
 var percent;
 
-//this variable will be used to calculate the variables heightInPixels and widthInPixels
+//this variable will be used to calculate the variables height into pixels and width into pixels
 var standardDPI = 72; // This is the standard for web that is able to be used by mac and windows computers.
 
 //this calculation will be used to help calculate the pixels in the final calculation. This expression will take the percentage and turn it into a decimal so it can be used in a calculation.
 percent /= 100;
+
+//to calculate the height in pixels you must multiply the height by standardDPI
+height *= standardDPI;
