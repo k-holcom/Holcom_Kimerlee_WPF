9370065f17e15375c3f2f87ab1283d0d86150412
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 14:42:42 -0500

    Givens declared
    These are the variables that will be either asked for or will be
    calculated during this project.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,11 @@
  */
 
 // Calculating the total number of pixels in a percentage of a workspace when given the customary (US) measurements
+
+//declaring the variables needed
+//the variables needed are height, width, pixels for each, and percentage that the user wants to know the number of pixels of.
+var height;
+var width;
+var heightInPixels;
+var widthInPixels;
+var percent;
