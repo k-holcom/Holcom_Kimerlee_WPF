08ad21fb01e6bf254883d5f3ba048568c0ab28a8
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-02 20:30:32 -0500

    Starting Expressions Worksheet
    Completed Dog Years problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,3 +3,14 @@
  * November 1, 2014
  * Expressions Worksheet
  */
+//Dog Years Problem.
+//declaring given variable
+var sparkyAge = 3;
+//declaring result variable
+var ageInDogYears;
+//calculating Sparky's age in dog years.
+ageInDogYears = sparkyAge * 7;
+//printing to the console
+//using concatenating to create the sentence "Sparky is X human years old which is X in dog years."
+console.log("Sparky is " + sparkyAge + " human years old which is " + ageInDogYears + " in dog years.");
+
