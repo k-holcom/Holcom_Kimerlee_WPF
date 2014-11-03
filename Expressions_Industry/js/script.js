ab20879826a91072036546225a35da8237cd404c
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 14:04:25 -0500

    checked all the calculations
    I checked all the calculations to see if they were all correct.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -17,6 +17,8 @@ height = prompt("What is the height of your web space (in inches)?");
 //console.log(height);
 //declaring height in pixels
 var pixelsHeight = height * standardDPI;
+//printing out the height in pixels
+//console.log(pixelsHeight);
 
 //asking for width
 width = prompt("What is the width of your web space (in inches)?");
@@ -24,10 +26,13 @@ width = prompt("What is the width of your web space (in inches)?");
 //console.log(width);
 //declaring/defining width in pixels;
 var pixelsWidth = width * standardDPI;
-//declaring/defining area in pixels
-var areaPixels = pixelsHeight * pixelsWidth;
-
+//printing out the width in pixels (just checking calculations)
+//console.log(pixelsWidth);
 //asking for the percentage they want to know.
 percent = prompt("What percentage of the screen do you want to know the area of?");
 //checking to see if percent was captured
 //console.log(percent);
+//declaring/defining area in pixels
+var areaPixels = pixelsHeight * pixelsWidth;
+//checking the calculations of the area
+//console.log(areaPixels);
