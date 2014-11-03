c039b09f112f0ba1de55cf1926e329a6db41539e
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-02 20:32:09 -0500

    Slice of Pie Part I
    Finished Slice of Pie Part I.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -14,3 +14,15 @@ ageInDogYears = sparkyAge * 7;
 //using concatenating to create the sentence "Sparky is X human years old which is X in dog years."
 console.log("Sparky is " + sparkyAge + " human years old which is " + ageInDogYears + " in dog years.");
 
+//Slice of Pie part I
+//declaring given variables
+var slicesPerPizza = 9;
+var numOfPeople = 7;
+var numOfPizzas = 5;
+//Declaring result variable
+var slicesPerPerson;
+//Calculating how many slices each person gets
+slicesPerPerson = (numOfPizzas * slicesPerPizza)/numOfPeople;
+//printing to console
+//using concatenating to create the sentence "Each person ate X slices of pizza at the party."
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
