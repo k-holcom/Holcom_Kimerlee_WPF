ae3ab3c07a6e85e402e7882fc224e23cd26ccf06
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-02 20:03:33 -0500

    Casting and Assignment Operators
    Learning how to change strings -> numbers and numbers -> strings. Also
    learned different ways of doing arithmetic.

---
+++ Screencasts/Assignment Operators/js/script.js
@@ -0,0 +1,12 @@
+//Expressions - Assignment Operators
+
+var a = 3;
+
+//a += 4; // same as a = a + 4
+//a *= 4; // same as a = a * 4
+//a /= 4; // same as a = a / 4
+//a -= 4; // same as a = a - 4
+//a++; //same as a = a + 1 or a += 1
+a--; //same as a = a - 1 or a -= 1
+
+console.log(a);
