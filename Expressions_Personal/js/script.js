0895ab8cf9c37fc2a500cc523571dd78975884d3
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 21:17:07 -0500

    Result Variables
    I declared the result variables cupsPerYear and futureNumOfCups

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,7 +3,7 @@
  * November 3, 2014
  * Expressions - Personal
  */
-//Using this to project to determine how many cups of coffee a person drinks in one year and then X number of years into the future.
+//Using this to project to determine how many cups of coffee a person drinks in one year and then X number of years into the future if trend continues.
 
 //declaring the variables that are needed for the calculation.
 var cupsPerDay;
@@ -12,3 +12,7 @@ var yearsIntoFuture;
 
 //declaring non-needed variables
 var name;
+
+//declaring the result variables
+var cupsPerYear;
+var futureNumOfCups;
