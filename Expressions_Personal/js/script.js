2a6b5b911c46184a43d9d07e3907cd8075f9d7df
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 21:01:58 -0500

    Description
    Description of Project.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,6 @@
  * November 3, 2014
  * Expressions - Personal
  */
+//Using this to project to determine how many cups of coffee a person drinks in one year and then X number of years into the future.
+
+
