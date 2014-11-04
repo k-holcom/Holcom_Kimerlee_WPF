0b9ff975100415806e343636b18b6c4bcef81b48
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-03 21:02:42 -0500

    variable declarations
    Here I declared the 3 main given variables.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,4 +5,7 @@
  */
 //Using this to project to determine how many cups of coffee a person drinks in one year and then X number of years into the future.
 
-
+//declaring the variables that are needed for the calculation.
+var cupsPerDay;
+var daysPerWeekCoffeeDrank;
+var yearsIntoFuture;
