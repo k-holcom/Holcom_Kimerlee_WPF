f37af10b0d2c45b8e9dc8ad7b3fa1944055884e1
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-06 21:30:57 -0500

    starting to defining array elements
    Starting to define the array elements by prompting user for the elements

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,7 +4,8 @@
  * Expressions - Wacky
  */
 
-//calculate the number of note cards it would take to cover a wall
+//calculate the number of note cards it would take to cover a wall and to cover the perimeter of the wall
 
 var measurements = [];
 
+measurements[0] = prompt("What is the width of your wall?");
