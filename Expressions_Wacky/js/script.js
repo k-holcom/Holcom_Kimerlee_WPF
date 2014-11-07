0913f93b7f7290a57bead283864fbd476ca4344c
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-06 21:41:35 -0500

    testing to array
    making sure the array elements were entered in correctly.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,5 +7,11 @@
 //calculate the number of note cards it would take to cover a wall and to cover the perimeter of the wall
 
 var measurements = [];
-
+//defining array elements
 measurements[0] = prompt("What is the width of your wall?");
+measurements[1] = prompt("What is the height of your wall?");
+measurements[2] = prompt("What is the width of your note card?");
+measurements[3] = prompt("What is the height of your note card?");
+
+//testing to see if the values go into the array
+console.log(measurements);
