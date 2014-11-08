c9a52f6ae19addc9fe693ea97f4cedc556bc4950
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-08 14:27:12 -0500

    conditionals with expressions
    can add numbers or variables to make it  condition with an expression.

---
+++ Screencasts/Conditionals/Condition with Expression/js/script.js
@@ -0,0 +1,11 @@
+//conditional logic -  with an expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console, "you can ride!"
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
