223c01096c1378a5305ee25902780193c391b964
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-08 14:43:38 -0500

    if/else
    if/else statements learned

---
+++ Screencasts/Conditionals/If and Else/js/script.js
@@ -0,0 +1,13 @@
+//conditional logic -  with an expression
+
+var kidHeight = 52;
+var minHeight = 48;
+
+//if the child is old enough, print to the console, "you can ride!"
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you got some growin' to do!");
+}
