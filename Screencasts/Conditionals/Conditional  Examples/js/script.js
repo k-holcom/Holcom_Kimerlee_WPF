2917f10b7659e03fe65e8b2af1ff48164b2f7e68
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-08 14:10:18 -0500

    Moved folders/New screencasts
    Moved the folders around to a folder that indicates which “week” they
    refer to. Also started the screencasts for Conditional statements.

---
+++ Screencasts/Conditionals/Conditional  Examples/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by KAHolcom on 11/8/14.
+ */
+//conditional logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console, "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
