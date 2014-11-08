0f69f1ae14589e967ac82f81a7865d6067b3203b
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-08 14:17:56 -0500

    relational expressions
    how to compare variables to others or to numbers

---
+++ Screencasts/Conditionals/Relational Expressions/js/script.js
@@ -0,0 +1,10 @@
+//conditional logic
+
+var kidHeight = 50;
+var minHeight = 48
+
+//if the child is old enough, print to the console, "you can ride!"
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
