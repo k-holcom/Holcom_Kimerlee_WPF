558b0bf4b31181082081d09f0319f03a35388de0
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-12 17:05:42 -0500

    validation
    created the if/else if statements to see if the validation is working
    correctly.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -15,3 +15,13 @@ startTime = prompt("In military time (i.e 600 for 6am, or 1500 for 3pm), what ti
 hoursWorked = prompt("How many hours do you work a day?");
 dinnerPrep = prompt("How long, in minutes, does it take to cook dinner?");
 
+// if startTime, hoursWorked, or dinnerPrep are empty, "You forgot something. Please enter a value.
+if(startTime == ""){
+    startTime = prompt("Sorry, you forgot to enter your start time. Please enter it in military time.");
+}else if(hoursWorked == ""){
+    hoursWorked = prompt("Sorry, you forgot to enter the number of hours you work a day. Please enter a value.");
+}else if(dinnerPrep == ""){
+    dinnerPrep = prompt("Sorry, you forgot to enter how long it takes to get dinner ready. Please enter a value in minutes.");
+}else{
+    console.log("validation is working.")
+}
