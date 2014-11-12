33a1bd8f240853b3e5b548bb42c016c4e32a0cce
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-12 17:05:04 -0500

    created variables
    created the 3 main variables and asked the user for the input.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,15 @@
  * November 12, 2014
  * Conditionals_Personal Assignment
  */
+//determining what time dinner is.
+
+//declaring given variables
+var hoursWorked;
+var startTime;
+var dinnerPrep;
+
+//collecting information
+startTime = prompt("In military time (i.e 600 for 6am, or 1500 for 3pm), what time do you start work?");
+hoursWorked = prompt("How many hours do you work a day?");
+dinnerPrep = prompt("How long, in minutes, does it take to cook dinner?");
+
