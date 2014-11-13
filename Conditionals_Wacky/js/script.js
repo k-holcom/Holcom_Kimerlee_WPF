2adc63c5cd18bd6d450ded2b2c53c9290be38fe7
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-13 14:01:49 -0500

    checking validation
    checking to see if the validation is correct.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,3 +12,12 @@ var numOnEachTeam;
 numOfPeople = prompt("How many people are there in all?");
 numOnEachTeam = prompt("How many people are needed for each team?");
 
+//validation
+//if numOfPeople is empty or is less than one, then asked to re-enter value
+if(numOfPeople === "" || numOfPeople < 1){
+    numOfPeople = prompt("Sorry, you either entered no answer or an answer less than 1. Please enter a valid number:");
+}
+//if numOnEachTeam is empty or is less than one, then asked to re-enter value.
+if(numOnEachTeam === "" || numOnEachTeam < 1){
+    numOnEachTeam = prompt("Sorry, you either entered no answer or an answer less than 1. Please enter a valid number:");
+}
