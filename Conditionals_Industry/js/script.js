01155c80928c2f1e37ea76e41bbf5acafd5d830b
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-13 11:51:16 -0500

    declaring/prompting
    Declaring the variables and using prompts to get the information from
    the user.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,10 @@
  * Conditionals - Industry Assignment
  */
 //determining if a website will need an e-commerce aspect to it
+//declaring the variable
+var businessOrPersonal;
+var sellingMerchandise;
+
+//asking User for the information needed
+businessOrPersonal = prompt("What kind of website are you looking to create (business or personal)? ");
+sellingMerchandise = prompt("Are you going to be selling merchandise (yes or no)?");
