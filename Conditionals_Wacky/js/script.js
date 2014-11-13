4a84b0b902ee09a48c883ba85cce5d1496fc4f76
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-13 14:28:41 -0500

    entered comments
    Entered comments to clear up code and to explain everything.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -28,14 +28,17 @@ var numOfTeams =(numOfPeople / numOnEachTeam);
 var numOfPeopleLeft = numOfPeople % numOnEachTeam;
 
 
-//if there are 0 or 1 team(s) then there are not enough people to play as teams
+//if there are 0 or 1 team(s) then there are not enough people to play as teams. Using Math.floor() function to round down decimal.
 if(Math.floor(numOfTeams) == 0 || Math.floor(numOfTeams) == 1){
     console.log("There are not enough people to play as teams. Find another game.");
     alert("There are not enough people to play as teams. Find another game.");
+//if there are more than 2 teams, then must decide if there are any people left over after dividing teams evenly
 }else {
+    //If there are any people left over, then list number of teams and number of people without a team.
     if (numOfPeopleLeft >= 1) {
-        console.log("There are enough people to play as " + Math.floor(numOfTeams) + " teams but there are " + numOfPeopleLeft + " people without a team.");
-        alert("There are enough people to play as " + Math.floor(numOfTeams) + " teams but there are " + numOfPeopleLeft + " people without a team.");
+        console.log("There are enough people to play as " + Math.floor(numOfTeams) + " teams but there are " + numOfPeopleLeft + " person/people without a team.");
+        alert("There are enough people to play as " + Math.floor(numOfTeams) + " teams but there are " + numOfPeopleLeft + " person/people without a team.");
+    //if there are no people left over the teams are even.
     } else {
         console.log("There are enough people to play as " + Math.floor(numOfTeams) + " teams.");
         alert("There are enough people to play as " + Math.floor(numOfTeams) + " teams.");
