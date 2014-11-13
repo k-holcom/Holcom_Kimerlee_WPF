1fba5731c4f3e4cf8da642fb227f592b6e7991ce
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-13 11:55:06 -0500

    validation
    validation coded, and it is working correctly.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,3 +11,13 @@ var sellingMerchandise;
 //asking User for the information needed
 businessOrPersonal = prompt("What kind of website are you looking to create (business or personal)? ");
 sellingMerchandise = prompt("Are you going to be selling merchandise (yes or no)?");
+
+
+//validation
+//if empty i.e. "" ask for prompt again
+if(businessOrPersonal === ""){
+    businessOrPersonal = prompt("Sorry, you left this blank, please enter if you want a personal or business website.");
+}
+if(sellingMerchandise === ""){
+    sellingMerchandise = prompt("Sorry, you left this blank, please enter whether you are selling merchandise (yes or no).");
+}
