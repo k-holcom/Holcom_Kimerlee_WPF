a5d42e4d709fa067002d8d1a1b7ba7288b023a98
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 18:23:17 -0500

    variables for worksheet
    declaring variables and using prompts

--- Functions_worksheet/js/functions.js
+++ Functions_worksheet/js/functions.js
@@ -9,34 +9,3 @@
 //declaring variable and getting info from user
 var rad = prompt("What is the radius of the circle?");
 
-//declaring the function
-var circumference  = function(radius){
-    // calculating cirucmference
-    var circumference = (2 * radius) * 3.14;
-    //returning the circumference.
-    return circumference;
-}
-
-//creating a variable to hold the
-var c = circumference(rad);
-
-//printing the result
-console.log(c);
-
-//Stung!
-//declaring variable and getting user information
-var weight = prompt("What is the victim's weight (in pounds)?");
-
-//defining the function
-function stung(weight){
-    //calculating number of stings
-    var numOfStings = weight * 8.666666667;
-    //returning numOfStings
-    return numOfStings;
-}
-
-//putting result into a variable
-var stings = stung(weight);
-
-//printing to console
-console.log(stings);
