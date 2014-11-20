d36e7b425a9a6037be9731e092ac50e42b9bffd2
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 16:39:40 -0500

    variables/prompt
    declared variables and used the prompts to get information

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,9 @@
 * November 20, 2014
 * Functions - Wacky
  */
+
+//how many minutes are in a month and how many are left in the month.
+//declaring variables
+
+var daysInMonth = prompt("How many days are in the current month?");
+var dayOfTheMonth = prompt("What is the current day of the month (i.e. 15, 30, etc)?");
