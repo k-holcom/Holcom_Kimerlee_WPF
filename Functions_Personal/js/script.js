6f05ca70dc2841d9033f9b5a00e15dcefa5985d7
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 17:37:52 -0500

    declaring variables
    declared the variables and used prompts for information

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,3 +3,9 @@
 * November 20, 2014
 * Functions - Personal
  */
+
+//calculating whether someone needs to buy a 10 lb bird or something bigger.
+
+var numOfGuests = prompt("How many guests are you expecting for thanksgiving?");
+var howManyEatLeftovers = prompt("How many of your guests will eat leftovers?");
+
