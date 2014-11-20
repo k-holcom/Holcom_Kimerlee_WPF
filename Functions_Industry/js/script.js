7f4f1e22cb8fb1aff120c34cd0d2b50e631966de
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 15:43:48 -0500

    added the while loop
    added the while loop to keep checking to see if the variables are empty
    strings

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,13 +10,14 @@ var budget = prompt("What is your budget (in dollars)?");
 var logo = prompt("Do you need a logo?");
 var numberOfPages = prompt("How many pages do you need?");
 
-if(budget === ""){
-    budget = prompt("Sorry you didn't enter a budget. Please enter that now.")
+while(budget === "" || logo === "" || numberOfPages === "") {
+    if (budget === "") {
+        budget = prompt("Sorry you didn't enter a budget. Please enter that now.")
+    }
+    if (logo === "") {
+        logo = prompt("Do you need a logo? (Yes or No)")
+    }
+    if (numberOfPages === "") {
+        numberOfPages = prompt("Please enter the number of pages you need.");
+    }
 }
-if(logo === ""){
-    logo = prompt("Do you need a logo? (Yes or No)")
-}
-if(numberOfPages === ""){
-    numberOfPages = prompt("Please enter the number of pages you need.");
-}
-
