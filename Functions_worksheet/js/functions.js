4c4522c42fd403ad2e86f5d1805d239f14f99154
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 18:24:34 -0500

    calculating circumference
    calculating circumference

--- Functions_worksheet/js/functions.js
+++ Functions_worksheet/js/functions.js
@@ -11,5 +11,7 @@ var rad = prompt("What is the radius of the circle?");
 
 //declaring the function
 var circumference  = function(radius){
-
+    // calculating cirucmference
+    var circumference = (2 * radius) * 3.14;
 }
+
