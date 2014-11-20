1dbd02f36c3d84a1751c8cf01af050d7bb09de93
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 16:43:27 -0500

    validation
    first crack at validating the prompts

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,3 +9,12 @@
 
 var daysInMonth = prompt("How many days are in the current month?");
 var dayOfTheMonth = prompt("What is the current day of the month (i.e. 15, 30, etc)?");
+
+while( daysInMonth === "" || daysInMonth < 28 || daysInMonth > 31 || dayOfTheMonth === "" || dayOfTheMonth < 1 || dayOfTheMonth > 31){
+    if(daysInMonth === "" || daysInMonth < 28 || daysInMonth > 31){
+        daysInMonth = prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 28 and 31.")
+    }
+    if(dayOfTheMonth === "" || dayOfTheMonth < 1 || dayOfTheMonth > 31){
+        daysInMonth = prompt(prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 1 and 31."))
+    }
+}
