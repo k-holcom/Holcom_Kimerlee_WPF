a32135d1a6ac0ce2e4495f7816bdd1d76ca6685f
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 16:46:46 -0500

    few changes to validation
    because of copy/pasting, needed to make a few changes to the second
    validation statement.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -12,9 +12,9 @@ var dayOfTheMonth = prompt("What is the current day of the month (i.e. 15, 30, e
 
 while( daysInMonth === "" || daysInMonth < 28 || daysInMonth > 31 || dayOfTheMonth === "" || dayOfTheMonth < 1 || dayOfTheMonth > 31){
     if(daysInMonth === "" || daysInMonth < 28 || daysInMonth > 31){
-        daysInMonth = prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 28 and 31.")
+        daysInMonth = prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 28 and 31.");
     }
     if(dayOfTheMonth === "" || dayOfTheMonth < 1 || dayOfTheMonth > 31){
-        daysInMonth = prompt(prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 1 and 31."))
+        dayOfTheMonth = prompt("You either forgot to enter something, or entered and invalid number. Please enter a number between 1 and 31.");
     }
 }
