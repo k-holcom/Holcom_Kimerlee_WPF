6e6400feb8fe07efbf1385f13b2e03bf4058bbe5
Kimerlee Holcom <kaholcom@fullsail.edu>
2014-11-20 14:50:56 -0500

    declaring variables
    declaring the variables and the prompts to get the information.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,9 @@
 * November 20, 2014
 * Functions - Industry
  */
+//Does the customer's budget fit what they want?
+
+//variable declaration/prompts
+var budget = prompt("What is your budget (in dollars)?");
+var logo = prompt("Do you need a logo?");
+var numberOfPages = prompt("How many pages do you need?");
